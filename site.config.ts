4f494af6eb07c3ab15f6c0f260e427c2a0d111a2
EveeeMo <30685619+EveeeMo@users.noreply.github.com>
2024-08-06 10:17:14 +0800

    change page id, name, author

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'https://sumptuous-chiller-fb3.notion.site/Hi-Welcome-to-Eve-s-Garden-9057588d1f0744d2a679873051d6e84a?pvs=4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: "Eve's Garden",
+  domain: '',
+  author: 'Eve Mo',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
